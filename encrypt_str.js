b066b1d7aa2daa80045ba9dfec73930c89a60052
melihsahtulek <melihsahtulek@gmail.com>
2023-01-01 12:08:04 +0300

    girilen string veriyi şifreler

--- encrypt_str.js
+++ encrypt_str.js
@@ -1,17 +1,38 @@
 function encrypt(str) {
   str = str.replace(/\s{2,}/gi, " ").trim();
+
+  if (!/^[a-zA-Z0-9._\s]+$/gi.test(str)) return "Please remove speacial chars.";
+
   let n = 4;
+  let count = 0;
   const result = {
     str: str,
     encrypted: "",
   };
-  if (str.length > 15) return "max length must be 15 letters.";
+
   for (let i = 0; i < str.length; i++) {
-    result.encrypted += String.fromCharCode(str.charCodeAt(i) + n);
+    let chr = str.charCodeAt(i);
+    chr = chr + n;
+
+    if (chr >= 65 && chr <= 90) {
+      if (chr > 90) {
+        chr = 65 + count + n;
+      }
+    } else if (chr >= 97 && chr <= 122) {
+      if (chr > 122) {
+        chr = 97 + count + n;
+      }
+    } else if (chr >= 48 && chr <= 57) {
+      if (chr > 57) {
+        chr = 48 + count + n;
+      }
+    }
+
+    count++;
+    result.encrypted += String.fromCharCode(chr);
   }
 
   return result;
 }
 
-console.log(encrypt("       Hello my  fr         s"));
-console.log(encrypt("xyz"));
+console.log(encrypt("MelihsahTulek"));
